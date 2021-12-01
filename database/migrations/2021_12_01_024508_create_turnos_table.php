dc6a35879a6188b1592637e9dbdc05094672e81e
MatheusABC <matheuscantarutti@gmail.com>
2021-11-30 23:48:10 -0300

    migrations criação da tabela turnos

--- database/migrations/2021_12_01_024508_create_turnos_table.php
+++ database/migrations/2021_12_01_024508_create_turnos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateTurnosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('turnos', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
+            $table->time("hora_inicio");
+            $table->time("hora_fim");
+            $table->smallInteger("qtd_horas_diurnas");
+            $table->smallInteger("qtd_horas_noturnas");
         });
     }
 
@@ -31,6 +30,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('turnos');
     }
 }
